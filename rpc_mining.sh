d964f5f69e1969f012900e7f0732f293bcacc982
hieutor <153083155+hieutor@users.noreply.github.com>
2023-12-09 20:16:31 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qy3fedygg56jnf5ge020pjuqhaxm0uqe7vrk5dczvur6gywk70tlkqg4r8ffr -r note8.cpumining.icloud:10100 -r community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -m6 -p rpc;
+    sleep 5;
+done
